b69d31a24cdc2e3ffa7a8f4b4a82be1cd6f721f6
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-01-19 23:32:04 -0500

    Spring Boot Project

---
+++ src/test/java/com/devrobot/springbootecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrobot.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
